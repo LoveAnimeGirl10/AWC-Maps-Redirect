65e4db4f1f1cb1fa013a68318272f57b123a214b
LoveAnimeGirl10 <loveanimegirl100@gmail.com>
2025-12-12 22:25:40 -0500

    Update redirects.js
    Updated link to Gacha Standard Challenge 5

--- redirects.js
+++ redirects.js
@@ -1,5 +1,5 @@
 // redirects.js
 const redirects = {
-  gacha_standard: "https://anilist.co/forum/thread/67067/comment/2884958",
+  gacha_standard: "https://anilist.co/forum/thread/67067/comment/3065912",
   gacha_mecha_girls: "https://anilist.co/forum/thread/67067/comment/2998428"
 };
