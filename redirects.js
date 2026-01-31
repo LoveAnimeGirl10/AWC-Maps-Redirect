445c3575ec6490a35f15d9dc3a0ec12d35005e9f
LoveAnimeGirl10 <loveanimegirl100@gmail.com>
2026-01-31 16:11:31 -0500

    Update redirects.js

--- redirects.js
+++ redirects.js
@@ -1,6 +1,6 @@
 // redirects.js
 const redirects = {
-  gacha_standard: "https://anilist.co/forum/thread/67067/comment/3065892",
-  gacha_mecha_girls: "https://anilist.co/forum/thread/67067/comment/3086638",
-  gacha_video_games: "https://anilist.co/forum/thread/67067/comment/3073526"
+  gacha_standard: "https://anilist.co/forum/thread/67067/comment/3065892",        // 5
+  gacha_mecha_girls: "https://anilist.co/forum/thread/67067/comment/3086638",     // 3
+  gacha_video_games: "https://anilist.co/forum/thread/67067/comment/3087168"      // 2
 };
