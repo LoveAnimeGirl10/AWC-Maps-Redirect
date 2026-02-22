4f48669bf78c1034d53d4bad19fec275db55935f
LoveAnimeGirl10 <loveanimegirl100@gmail.com>
2026-02-22 01:05:16 -0500

    Update redirects.js

--- redirects.js
+++ redirects.js
@@ -2,5 +2,5 @@
 const redirects = {
   gacha_standard: "https://anilist.co/forum/thread/67067/comment/3065892",        // 5
   gacha_mecha_girls: "https://anilist.co/forum/thread/67067/comment/3086638",     // 3
-  gacha_video_games: "https://anilist.co/forum/thread/67067/comment/3087168"      // 2
+  gacha_video_games: "https://anilist.co/forum/thread/67067/comment/3094628"      // 3
 };
