cdb1f05429221551f3af60608f01a1302ba3ff7d
LoveAnimeGirl10 <loveanimegirl100@gmail.com>
2026-03-18 15:16:38 -0400

    Update redirects.js

--- redirects.js
+++ redirects.js
@@ -2,5 +2,5 @@
 const redirects = {
   gacha_standard: "https://anilist.co/forum/thread/67067/comment/3065892",        // 5
   gacha_mecha_girls: "https://anilist.co/forum/thread/67067/comment/3095022",     // 4
-  gacha_video_games: "https://anilist.co/forum/thread/67067/comment/3094628"      // 3
+  gacha_video_games: "https://anilist.co/forum/thread/67067/comment/3131931"      // 4
 };
