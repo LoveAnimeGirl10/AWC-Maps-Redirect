6411c0a7413917e9b87494c92a0f51d81caeb293
LoveAnimeGirl10 <loveanimegirl100@gmail.com>
2026-07-03 18:39:15 -0400

    Add new redirect for gacha_surviving_highschool

--- redirects.js
+++ redirects.js
@@ -1,7 +1,8 @@
 // redirects.js
 const redirects = {
-  gacha_standard: "https://anilist.co/forum/thread/67067/comment/3165160",        // 6
-  gacha_mecha_girls: "https://anilist.co/forum/thread/67067/comment/3134241",     // 5
-  gacha_video_games: "https://anilist.co/forum/thread/67067/comment/3131931",     // 4
-  gacha_sports_festival: "https://anilist.co/forum/thread/67067/comment/3206190", // 4
+  gacha_standard: "https://anilist.co/forum/thread/67067/comment/3165160",                // 6
+  gacha_mecha_girls: "https://anilist.co/forum/thread/67067/comment/3134241",             // 5
+  gacha_video_games: "https://anilist.co/forum/thread/67067/comment/3131931",             // 4
+  gacha_sports_festival: "https://anilist.co/forum/thread/67067/comment/3206190",         // 4
+  gacha_surviving_highschool: "https://anilist.co/forum/thread/67067/comment/3213120",    // 1
 };
