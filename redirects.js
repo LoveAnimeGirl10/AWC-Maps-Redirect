902fe4986f9db207c4c534d3f8421deb4bcf5f56
LoveAnimeGirl10 <loveanimegirl100@gmail.com>
2026-07-12 17:26:08 -0400

    Update gacha_sports_festival redirect URL

--- redirects.js
+++ redirects.js
@@ -3,6 +3,6 @@ const redirects = {
   gacha_standard: "https://anilist.co/forum/thread/67067/comment/3165160",                // 6
   gacha_mecha_girls: "https://anilist.co/forum/thread/67067/comment/3134241",             // 5
   gacha_video_games: "https://anilist.co/forum/thread/67067/comment/3131931",             // 4
-  gacha_sports_festival: "https://anilist.co/forum/thread/67067/comment/3206190",         // 4
+  gacha_sports_festival: "https://anilist.co/forum/thread/67067/comment/3219420",         // 5
   gacha_surviving_highschool: "https://anilist.co/forum/thread/67067/comment/3213120",    // 1
 };
